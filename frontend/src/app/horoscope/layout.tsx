10fe33e0bc56d207db50775012cba302177b658a
aadil-nv <adilev2000@gmail.com>
2026-03-17 14:24:40 +0530

    second commit

--- frontend/src/app/horoscope/layout.tsx
+++ frontend/src/app/horoscope/layout.tsx
@@ -1,15 +1,7 @@
-import { Montserrat } from "next/font/google";
-
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-  variable: "--font-montserrat",
-});
-
 export default function HoroscopeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className={montserrat.className}>{children}</div>;
+  return <>{children}</>;
 }
